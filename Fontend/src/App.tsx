08d0cf9a32af24018dbe5b7ec5b35c3ad81127c8
Phadec <21130467@st.hcmuaf.edu.vn>
2024-07-10 23:33:24 +0700

    fix 1 số lỗi

--- Fontend/src/App.tsx
+++ Fontend/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Home from "./component/home/Home";
--- Server/src/server.tsx
+++ Server/src/server.tsx
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(cors());
+
 interface RssItem {
     title: string;
     link: string;
@@ -15,42 +16,6 @@ interface RssItem {
     textContent: string;
     pubDate: string;
 }
-
-// Render RSS
-app.get('/rss', async (req: Request, res: Response) => {
-    try {
-        const rssUrl = req.query.url as string;
-        if (!rssUrl) {
-            return res.status(400).json({ error: 'URL is required' });
-        }
-
-        const { data } = await axios.get(rssUrl);
-        parseString(data, (err: Error | null, result: any) => {
-            if (err) {
-                return res.status(500).json({ error: 'Error parsing RSS data' });
-            }
-
-            const items: RssItem[] = result.rss.channel[0].item.map((item: any) => {
-                const description = item.description[0];
-                const $ = cheerio.load(description);
-                const anchorTag = $('a').each(function (this:cheerio.Element) {
-                    $(this).removeAttr('href');
-                }).prop('outerHTML') || '';
-                const textContent = $('body').text().trim();
-                return {
-                    title: item.title[0],
-                    link: item.link[0],
-                    anchorTag,
-                    textContent,
-                    pubDate: item.pubDate[0],
-                };
-            });
-
-            res.json(items);
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch RSS data' });
 
 interface VideoData {
     videoUrl: string;
@@ -76,6 +41,44 @@ const extractVideoData = async ($: cheerio.Root): Promise<VideoData> => {
 
     return { videoUrl, videoData, videoCaption };
 };
+
+// Render RSS
+app.get('/rss', async (req: Request, res: Response) => {
+    try {
+        const rssUrl = req.query.url as string;
+        if (!rssUrl) {
+            return res.status(400).json({ error: 'URL is required' });
+        }
+
+        const { data } = await axios.get(rssUrl);
+        parseString(data, (err: Error | null, result: any) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error parsing RSS data' });
+            }
+
+            const items: RssItem[] = result.rss.channel[0].item.map((item: any) => {
+                const description = item.description[0];
+                const $ = cheerio.load(description);
+                const anchorTag = $('a').each(function (this: cheerio.Element) {
+                    $(this).removeAttr('href');
+                }).prop('outerHTML') || '';
+                const textContent = $('body').text().trim();
+                return {
+                    title: item.title[0],
+                    link: item.link[0],
+                    anchorTag,
+                    textContent,
+                    pubDate: item.pubDate[0],
+                };
+            });
+
+            res.json(items);
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch RSS data' });
+    }
+});
 
 app.get('/scrape', async (req: Request, res: Response) => {
     try {
